86e8def8567379ebba3b3cfc2c638666acbfaab4
rammelbalagtas <rammel.balagtas@gmail.com>
2022-03-24 20:28:49 -0400

    Replace the image view with a collection view and embed in navigation controller

--- PokiAPIGallery/MainViewController.swift
+++ PokiAPIGallery/MainViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     @IBOutlet weak var pokeimage: UIImageView!
     override func viewDidLoad() {
