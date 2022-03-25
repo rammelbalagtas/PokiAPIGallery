06ecc8cf9bdc18ad9de30ce7fa47b8663c83f25c
rammelbalagtas <rammel.balagtas@gmail.com>
2022-03-24 20:40:52 -0400

    Add a collection view cell and bind the image view

---
+++ PokiAPIGallery/PokemonCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PokemonCollectionViewCell.swift
+//  PokiAPIGallery
+//
+//  Created by Rammel on 2022-03-24.
+//
+
+import UIKit
+
+class PokemonCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var pokeImage: UIImageView!
+    
+}
