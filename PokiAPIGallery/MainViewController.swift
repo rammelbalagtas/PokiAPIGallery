4dfdd9c279a299b2c9e52111e67a0222e80fb138
rammelbalagtas <rammel.balagtas@gmail.com>
2022-03-24 20:36:03 -0400

    Add image view to collection view cell and assign view controller as collection view delegate

--- PokiAPIGallery/MainViewController.swift
+++ PokiAPIGallery/MainViewController.swift
@@ -7,15 +7,32 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource  {
+    
+    let pokemons = [Pokemon]()
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     @IBOutlet weak var pokeimage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //assign view controller as delegate for the collection view
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
         // Do any additional setup after loading the view.
         PokeAPIHelper.fetchAllImages { images in
             self.pokeimage.image = images.first!
         }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
     }
 
 
